611b0c505fc44cc0af5dbdb2e8327e8f11bdc98d
OlgaKovalenk0 <161139017+OlgaKovalenk0@users.noreply.github.com>
2024-08-23 23:02:58 +0300

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,10 +1,12 @@
 'use strict';
 
 window.addEventListener('scroll', event => {
-    const message = document.querySelector('div.reveal')
-    const massageTopX = message.getBoundingClientRect().top;
-    const massageBottomX = message.getBoundingClientRect().bottom;
-    const viewportHeight = window.innerHeight;
-    const condition = (massageTopX < (viewportHeight / 1.5) & massageBottomX >= (viewportHeight / 4))
-    condition ? message.classList.add('reveal_active') : message.classList.remove('reveal_active');
-})
+    const elements = document.querySelectorAll('.reveal');
+    elements.forEach((element) => {
+        const elementTopX = element.getBoundingClientRect().top;        
+        const viewportHeight = window.innerHeight;
+        if(elementTopX < viewportHeight) {
+            element.classList.add('reveal_active')
+        }
+    })
+});
